1a11f32a02b73c4b9a149ca7788ad8233d3d5231
Julien Aubin <julien.aubin@ebiznext.com>
2018-05-25 11:12:01 +0200

    updated launch script

--- dev_resources/test_app_dir/test_app/start_app.sh
+++ dev_resources/test_app_dir/test_app/start_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar "$(dirname $0)/target/spring-cloud-consul-1.0.0-SNAPSHOT-spring-boot.jar" --spring.config.location="$(dirname $0)/config/application.properties"
+java -jar "$(dirname $0)/target/spring-cloud-consul-spring-boot.jar" --spring.config.location="$(dirname $0)/config/application.properties"
